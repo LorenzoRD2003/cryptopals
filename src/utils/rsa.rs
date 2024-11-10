5c54b979ee2cdeaa81cb97f73a8a64124ab55c2d
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2024-11-10 11:55:55 -0300

    Completed Challenge 39. My primegen is very slow.

--- src/utils/rsa.rs
+++ src/utils/rsa.rs
@@ -1,34 +1,27 @@
 use num_bigint::BigUint;
 use num_traits::One;
-use rand::thread_rng;
+use crate::utils::algebra::{generate_prime, inv_mod, mod_exp};
 
-use crate::utils::algebra::{inv_mod, mod_exp};
-
-pub struct RSA2048Keys {
+pub struct RSA {
   pub sk: (BigUint, BigUint), // (d,n)
   pub pk: (BigUint, BigUint), // (e,n)
 }
 
-pub struct RSA2048 {}
-
-impl RSA2048 {
+impl RSA {
   const E: u8 = 3;
-  const BITS: usize = 2048;
+  const BITS: u64 = 2048;
+  const ITERATIONS: u64 = 7;
 
-  pub fn generate_keys() -> RSA2048Keys {
-    let mut rng = thread_rng();
+  pub fn generate_keys() -> Self {
     loop {
-      //let p: BigUint = rng.gen_safe_prime(Self::BITS);
-      //let q: BigUint = rng.gen_safe_prime(Self::BITS);
-      let (p, q) = (BigUint::from(17u32), BigUint::from(23u32));
+      let p: BigUint = generate_prime(Self::BITS, Self::ITERATIONS);
+      let q: BigUint = generate_prime(Self::BITS, Self::ITERATIONS);
       let n = &p * &q;
       let et = (&p - BigUint::one()) * (&q - BigUint::one());
       let option_d = inv_mod(&BigUint::from(Self::E), &et);
-      option_d.clone().unwrap();
-      println!("{}", option_d.is_none());
       match option_d {
         Some(d) => {
-          return RSA2048Keys {
+          return Self {
             sk: (d, n.clone()),
             pk: (BigUint::from(Self::E), n),
           };
@@ -53,30 +46,33 @@ impl RSA2048 {
 
 #[cfg(test)]
 mod tests {
+  use num_bigint::RandBigInt;
+  use rand::thread_rng;
   use super::*;
-  use crate::utils::algebra::get_nist_prime;
 
   #[test]
   fn test_rsa_small_numbers() {
-    let rsa_keys: RSA2048Keys = RSA2048::generate_keys();
-    let plaintext = 42u32.to_be_bytes().to_vec();
-    let ciphertext = RSA2048::encrypt(&rsa_keys.pk, &plaintext);
-    assert_eq!(plaintext, RSA2048::decrypt(&rsa_keys.sk, &ciphertext));
+    let rsa_keys = RSA::generate_keys();
+    let plaintext = 42u8.to_be_bytes().to_vec();
+    let ciphertext = RSA::encrypt(&rsa_keys.pk, &plaintext);
+    assert_eq!(plaintext, RSA::decrypt(&rsa_keys.sk, &ciphertext));
   }
 
   #[test]
   fn test_rsa_big_numbers() {
-    let rsa_keys: RSA2048Keys = RSA2048::generate_keys();
-    let plaintext = get_nist_prime().to_bytes_be();
-    let ciphertext = RSA2048::encrypt(&rsa_keys.pk, &plaintext);
-    assert_eq!(plaintext, RSA2048::decrypt(&rsa_keys.sk, &ciphertext));
+    let rsa_keys = RSA::generate_keys();
+    let mut rng = thread_rng();
+    // Remember it should be sized <= 2048 bits, if not, separate in chunks
+    let plaintext = rng.gen_biguint(1024).to_bytes_be();
+    let ciphertext = RSA::encrypt(&rsa_keys.pk, &plaintext);
+    assert_eq!(plaintext, RSA::decrypt(&rsa_keys.sk, &ciphertext));
   }
 
   #[test]
   fn test_rsa_text() {
-    let rsa_keys: RSA2048Keys = RSA2048::generate_keys();
+    let rsa_keys: RSA = RSA::generate_keys();
     let plaintext = b"SOY BOSTERO DE LA CUNA A LA TUMBA Y NUNCA DESCENDERE".to_vec();
-    let ciphertext = RSA2048::encrypt(&rsa_keys.pk, &plaintext);
-    assert_eq!(plaintext, RSA2048::decrypt(&rsa_keys.sk, &ciphertext));
+    let ciphertext = RSA::encrypt(&rsa_keys.pk, &plaintext);
+    assert_eq!(plaintext, RSA::decrypt(&rsa_keys.sk, &ciphertext));
   }
 }
